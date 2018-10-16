32b0b5137f67211bd22ead2525f4a9cc182c3add
mohamedaraby122 <mohamedaraby1296@gmail.com>
2018-10-17 01:51:20 +0200

    done

---
+++ Climp/Assets/scripts/GripManger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripManger : MonoBehaviour {
+    public Rigidbody player;
+    public climp left, right;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void FixeedUpdate()
+    {
+        var ldevice = SteamVR_Controller.Input((int)left.controller.index);
+        var rdevice = SteamVR_Controller.Input((int)left.controller.index);
+        bool isgrip = left.cangrip || right.cangrip;
+        if (isgrip)
+        {
+            if (left.cangrip && ldevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                player.useGravity = false;
+                player.isKinematic = true;
+                player.transform.position += (left.prevpos - left.controller.transform.localPosition);
+            }
+            else if (left.cangrip && ldevice.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                player.useGravity = true;
+                player.isKinematic = false;
+                player.velocity = 2f * (left.prevpos - left.controller.transform.localPosition) / Time.deltaTime;
+            }
+            //////////////////// RIght
+            if (right.cangrip && rdevice.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                player.useGravity = false;
+                player.isKinematic = true;
+                player.transform.position += (right.prevpos - right.controller.transform.localPosition);
+            }
+            else if (right.cangrip && rdevice.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                player.useGravity = true;
+                player.isKinematic = false;
+                player.velocity = 2f * (right.prevpos - right.controller.transform.localPosition) / Time.deltaTime;
+            }
+        }
+        else
+        {
+            player.useGravity = true;
+            player.isKinematic = false;
+        }
+        left.prevpos = left.controller.transform.localPosition;
+        right.prevpos = right.controller.transform.localPosition;
+        if (ldevice.GetTouch(SteamVR_Controller.ButtonMask.Grip) || rdevice.GetTouch(SteamVR_Controller.ButtonMask.Grip))
+        {
+            player.transform.position = new Vector3(30.68f, 1.05f, 50.73f);
+        }
+    }
+       
+    }
+
---
+++ Climp/Assets/scripts/climp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class climp : MonoBehaviour
+{
+    public SteamVR_TrackedObject controller;
+    [HideInInspector]
+    public Vector3 prevpos;
+    [HideInInspector]
+    public bool cangrip;
+    // Use this for initialization
+    void Start()
+    {
+      
+        prevpos = controller.transform.localPosition;
+    }
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        cangrip = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        cangrip = false;
+    }
+}
